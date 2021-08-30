30a1221876f7218d4b40347044a66227095b8d88
BlockJJam <blockjjam99@gmail.com>
2021-08-30 21:51:37 +0900

    feat: Form 전송 객체 분리를 통한 Validation
    Validation groups를 통해 하나의 객체에 여러 쓰임새를 나눠 검증을 해보았지만, 결국 유지보수의 난이도가 더욱 높아진다는 것을 확인, Form 전송객체(dto로 나중에 진화하게 되는)에서 검증로직을 구현하여 상황에 맞는 검증 객체를 구현하여 검증해본다

---
+++ src/main/java/hello/itemservice/web/validation/ValidationItemApiController.java
@@ -0,0 +1,10 @@
+package hello.itemservice.web.validation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.validator.constraints.Range;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+public class ValidationItemApiController {
+}
